48f1a121ae251b5a7ff2cc8569688039841033c4
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-04-30 05:28:16 +0100

    Added Networked Player Movement

---
+++ Assets/Scripts/LookX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+
+    [SerializeField] float xSensitivity = 5.0f;
+
+    [SerializeField] float maxX = 80f;
+    [SerializeField] float minX = -80f;
+
+    float xRot;
+    Quaternion currentRotation;
+
+    private void Start()
+    {
+        currentRotation = transform.localRotation;
+    }
+
+    void Update()
+    {
+        xRot += Input.GetAxis("Mouse Y") * xSensitivity;
+        xRot = Mathf.Clamp(xRot, minX, maxX);
+
+        Quaternion xQuaternion = Quaternion.AngleAxis(-xRot, Vector3.right);
+        transform.localRotation = currentRotation * xQuaternion;
+    }
+}
---
+++ Assets/Scripts/LookY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField] float ySensitivity = 5f;
+
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, Input.GetAxis("Mouse X") * ySensitivity, 0);
+    }
+}
