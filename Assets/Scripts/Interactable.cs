ffe44ceac20c71d729ff0a7f88e8c6ca25ec0ee6
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-01 18:23:43 +0100

    Added Player Interaction

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnInteract;
+
+    public bool isInteractable = true;
+    public void Interact()
+    {
+        OnInteract.Invoke();
+
+        Debug.Log(gameObject.name + " Interacted With");
+
+        Interacted();
+    }
+
+    public virtual void Interacted()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float interactDistance = 1.0f;
+    [SerializeField] LayerMask interactLayer;
+    Interactable currentInteract;
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawLine(cam.transform.position, cam.transform.position + (cam.transform.forward * interactDistance), Color.red);
+
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactDistance, interactLayer))
+        {
+            if(hit.transform.GetComponent<Interactable>())
+            {
+                currentInteract = hit.transform.GetComponent<Interactable>();
+
+                if(currentInteract.isInteractable)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                currentInteract = null;
+            }
+        }
+        else
+        {
+            currentInteract = null;
+        }
+    }
+
+    void OnInteract()
+    {
+        if (currentInteract != null)
+        {
+            if (currentInteract.isInteractable)
+            {
+                currentInteract.Interact();
+            }
+        }
+    }
+}
