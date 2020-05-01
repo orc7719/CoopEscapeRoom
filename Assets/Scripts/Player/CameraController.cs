547160457cd2c5de56adf061b2261a7602a2bebd
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-01 18:23:22 +0100

    Setup Networked Player Movement

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+
+    [SerializeField] float ySensitivity = 1.0f;
+
+    [SerializeField] float xSensitivity = 1.0f;
+    [SerializeField] float minX = -80.0f;
+    [SerializeField] float maxX = 80.0f;
+
+    float xRot;
+    Quaternion currentRotation;
+
+
+    private void Start()
+    {
+        currentRotation = cam.transform.localRotation;
+    }
+
+    void OnLookX(InputValue input)
+    {
+        //Debug.Log("Look X: " + input.Get<float>());
+        transform.eulerAngles += new Vector3(0, input.Get<float>() * ySensitivity, 0);
+    }
+
+    void OnLookY(InputValue input)
+    {
+        //Debug.Log("Look Y: " + input.Get<float>());
+
+        xRot += input.Get<float>() * xSensitivity;
+        xRot = Mathf.Clamp(xRot, minX, maxX);
+
+        Quaternion xQuaternion = Quaternion.AngleAxis(-xRot, Vector3.right);
+        cam.transform.localRotation = currentRotation * xQuaternion;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,139 +1,61 @@
 ﻿using UnityEngine;
-using Mirror;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
-/*
-	Documentation: https://mirror-networking.com/docs/Guides/NetworkBehaviour.html
-	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
-*/
-
-public class PlayerMovement : NetworkBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 10.0f;
-    [SerializeField] float gravity = 10.0f;
-    [SerializeField] float maxVelocityChange = 10.0f;
-    [SerializeField] float jumpHeight = 2.0f;
+    [SerializeField] float speed = 6.0f;
+    [SerializeField] float maxVelocityChange = 8.0f;
+
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] float gravity = 9.81f;
+
+
     [SerializeField] LayerMask groundLayers;
-
-    Rigidbody rgdbody;
-    Vector3 targetVelocity;
-    Vector3 velocity;
-
-    #region Start & Stop Callbacks
-
-    /// <summary>
-    /// This is invoked for NetworkBehaviour objects when they become active on the server.
-    /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
-    /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
-    /// </summary>
-    public override void OnStartServer() { }
-
-    /// <summary>
-    /// Invoked on the server when the object is unspawned
-    /// <para>Useful for saving object data in persistant storage</para>
-    /// </summary>
-    public override void OnStopServer() { }
-
-    /// <summary>
-    /// Called on every NetworkBehaviour when it is activated on a client.
-    /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
-    /// </summary>
-    public override void OnStartClient() { }
-
-    /// <summary>
-    /// This is invoked on clients when the server has caused this object to be destroyed.
-    /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
-    /// </summary>
-    public override void OnStopClient() { }
-
-    /// <summary>
-    /// Called when the local player object has been set up.
-    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
-    /// </summary>
-    public override void OnStartLocalPlayer() { }
-
-    /// <summary>
-    /// This is invoked on behaviours that have authority, based on context and <see cref="NetworkIdentity.hasAuthority">NetworkIdentity.hasAuthority</see>.
-    /// <para>This is called after <see cref="OnStartServer">OnStartServer</see> and before <see cref="OnStartClient">OnStartClient.</see></para>
-    /// <para>When <see cref="NetworkIdentity.AssignClientAuthority"/> is called on the server, this will be called on the client that owns the object. When an object is spawned with <see cref="NetworkServer.Spawn">NetworkServer.Spawn</see> with a NetworkConnection parameter included, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStartAuthority() { }
-
-    /// <summary>
-    /// This is invoked on behaviours when authority is removed.
-    /// <para>When NetworkIdentity.RemoveClientAuthority is called on the server, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStopAuthority() { }
-
-    #endregion
-
+    private Rigidbody rgdbody;
+    private Vector2 inputVector;
+    private Vector3 fixedVelocity;
+    private Vector3 targetVelocity;
+    private Vector3 velocity;
 
     private void Awake()
     {
         rgdbody = GetComponent<Rigidbody>();
     }
 
-    private void Start()
+    void OnMovement(InputValue value)
     {
-        rgdbody.freezeRotation = true;
-        rgdbody.useGravity = false;
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Debug.Log("Moving: " + value.Get<Vector2>());
+        inputVector = value.Get<Vector2>();
+        fixedVelocity = new Vector3(inputVector.x, 0, inputVector.y);
     }
 
-    private void Update()
+    void OnJump()
     {
-        if (!isLocalPlayer)
-            return;
-
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-        if(Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-
-        if (isGrounded())
-        {
-            targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                rgdbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
-            }
-        }
+        if (IsGrounded())
+            rgdbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
     }
-
     private void FixedUpdate()
     {
-        if (!isLocalPlayer)
-            return;
-
-        if(isGrounded())
-        {
-            
-            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity = transform.TransformDirection(fixedVelocity);
             targetVelocity *= speed;
 
             velocity = rgdbody.velocity;
             Vector3 velocityChange = targetVelocity - velocity;
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
             velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            velocityChange.y = 0;
+            velocityChange.y = 0.0f;
+
             rgdbody.AddForce(velocityChange, ForceMode.VelocityChange);
-        }
+        
 
         rgdbody.AddForce(new Vector3(0, -gravity * rgdbody.mass, 0));
     }
 
-    bool isGrounded()
+    bool IsGrounded()
     {
-        return Physics.Raycast(transform.position + (transform.up * 0.1f), Vector3.down, 0.3f, groundLayers);
+        Ray ray = new Ray(transform.position + (Vector3.up * 0.25f), Vector3.down);
+        return Physics.SphereCast(ray, 0.2f, 0.1f, groundLayers);
     }
 
     float CalculateJumpVerticalSpeed()
