0d678a93ad7bf55ab113218d1f96e62bb9f9d21f
scadan <31972143+scadan@users.noreply.github.com>
2020-05-02 18:12:47 +0100

    Implemented the new WSAD movement from the New Input System

--- Assets/Single Player FPS Prefab/FpsController/NewInputWSAD.cs
+++ Assets/Single Player FPS Prefab/FpsController/NewInputWSAD.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class NewInputWSAD : MonoBehaviour
@@ -12,24 +13,49 @@ public class NewInputWSAD : MonoBehaviour
     //Move
     Vector2 movementInput;
 
+    Vector2 lookRotation;
+
+    Rigidbody player;
+
+    public float verticalSpeed, horizontalSpeed;
+
 
 void Awake() {
     playerInput = new SinglePlayerFPS();
 
     playerInput.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
 
+
+    
 }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         float h = movementInput.x;
         float v = movementInput.y;
+
+        if (h > 0) {
+            player.AddForce(transform.right * verticalSpeed);
+        }
+        else if (h < 0) {
+            player.AddForce(transform.right * -verticalSpeed);
+        }
+
+        if (v > 0) {
+            player.AddForce(transform.forward * horizontalSpeed);
+        }
+        else if (v < 0) {
+            player.AddForce(transform.forward * -horizontalSpeed);
+        }
+
+      
+
     }
 
     void OnEnable() {
@@ -39,4 +65,6 @@ void Awake() {
     void OnDisable() {
         playerInput.Disable();
     }
+
+    
 }
