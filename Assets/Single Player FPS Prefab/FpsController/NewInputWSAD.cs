9dea345d5cd85f97e74bd5b025da4d3f3dafaacb
scadan <31972143+scadan@users.noreply.github.com>
2020-05-02 16:58:48 +0100

    Reorganizing Prototype Scripts Folder
    I have also created a simple maze in my test scene to allow me to setup traps and/or other assets in this puzzle.
    
    Also trying to modify my singleplayer's movement script by using the new Unity's Input System

---
+++ Assets/Single Player FPS Prefab/FpsController/NewInputWSAD.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewInputWSAD : MonoBehaviour
+{
+
+    //Input Actions
+    SinglePlayerFPS playerInput;
+
+    //Move
+    Vector2 movementInput;
+
+
+void Awake() {
+    playerInput = new SinglePlayerFPS();
+
+    playerInput.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float h = movementInput.x;
+        float v = movementInput.y;
+    }
+
+    void OnEnable() {
+        playerInput.Enable();
+    }
+
+    void OnDisable() {
+        playerInput.Disable();
+    }
+}
