8bde6e86b5b3123701a8de3ed69396bdb9e15a5d
scadan <31972143+scadan@users.noreply.github.com>
2020-05-03 17:02:53 +0100

    Added Animation to the Door by Using DoTween Aniamtion System

---
+++ Assets/Scripts/Prototype Scripts/Maze Puzzle Scripts/doorDoTweenController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class doorDoTweenController : MonoBehaviour
+{
+
+    [Range(0.5f, 5f), SerializeField] private float _movementDuration = 1.0f;
+
+
+    private DoTweenType _doTweenType = DoTweenType.openDoor;
+
+    private enum DoTweenType {
+        openDoor
+        
+        
+    }
+
+    void OnEnable() {
+        if (_doTweenType == DoTweenType.openDoor) {
+            
+            
+            transform.DORotate(new Vector3(0, 0, 0), _movementDuration, RotateMode.Fast);
+        }
+    }
+}
+
+
