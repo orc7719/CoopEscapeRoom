b928d3a835b5cfd38b84c507f44c1360cb5da7e2
scadan <31972143+scadan@users.noreply.github.com>
2020-05-03 17:55:48 +0100

    Created a Lever Script
    I have created a lever script that allows a selected door to open when the user interacts with it. This is with an animation as well to the lever.

---
+++ Assets/Scripts/Prototype Scripts/Maze Puzzle Scripts/leverDoTweenAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class leverDoTweenAnimation : MonoBehaviour
+{
+    private float movementDuration = 1;
+
+    [SerializeField]GameObject lever, SelectedDoor;
+
+    Quaternion targetRotation;
+
+    void Start() {
+        
+    }
+
+    public void LeverActivated() {
+        lever = GameObject.Find("lever");
+
+        lever.transform.DORotateQuaternion(Quaternion.EulerAngles(0, -90, -90), movementDuration);
+
+        SelectedDoor.GetComponent<doorDoTweenController>().enabled = true;
+
+        //lever.transform.DOMove(new Vector3(0.539f, 0.000865f, -0.2644743f), movementDuration);
+    }
+}
