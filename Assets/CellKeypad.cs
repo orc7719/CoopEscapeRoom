dd4305bb2b3aa1a04c24a31628c3a6c2ec4b99c6
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-08 03:42:30 +0100

    Setup Keypad to Send Events Over Network

---
+++ Assets/CellKeypad.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using ScriptableObjectArchitecture;
+
+public class CellKeypad : MonoBehaviour
+{
+    [SerializeField] string correctPasscode = "0526";
+    string inputCode = "";
+
+    bool canInput = true;
+
+    [SerializeField] RawImage[] symbolDisplays;
+    [SerializeField] Texture[] symbols;
+
+    [SerializeField] GameEvent outputEvent;
+
+    private void Start()
+    {
+        inputCode = "";
+
+        for (int i = 0; i < symbolDisplays.Length; i++)
+        {
+            symbolDisplays[i].color = Color.clear;
+        }
+    }
+
+    public void InputCode(int newCode)
+    {
+        if (!canInput)
+            return;
+
+        symbolDisplays[inputCode.Length].color = Color.white;
+        symbolDisplays[inputCode.Length].texture = symbols[newCode];
+
+        inputCode += newCode;
+        
+
+        if(inputCode.Length >= 4)
+        {
+            if(inputCode == correctPasscode)
+            {
+                canInput = false;
+                NetworkedEventManager.instance.TriggerEvent("Symbols_KeypadCorrect");
+            }
+            else
+            {
+                inputCode = "";
+                for (int i = 0; i < symbolDisplays.Length; i++)
+                {
+                    symbolDisplays[i].color = Color.clear;
+                }
+            }
+        }
+    }
+}
