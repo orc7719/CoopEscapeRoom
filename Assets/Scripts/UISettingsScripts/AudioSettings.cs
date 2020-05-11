24efe181db9955c7e6480be751d1ce8983974db1
scadan <31972143+scadan@users.noreply.github.com>
2020-05-11 16:49:20 +0100

    Finished a Basic Audio Settings Page
    In the future we plan on using the volume mixer to make it easier to put sounds in different mixers. So that players can turn off the music while still having the sound effects of the game on max.
    
    However. Due to myself not being familiar with the Volume Mixer. For now, this will do and can setup the basis of a full colume setup

---
+++ Assets/Scripts/UISettingsScripts/AudioSettings.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+using TMPro;
+using UnityEngine.Audio;
+
+public class AudioSettings : MonoBehaviour
+{
+    [Header("The Sliders")]
+    public Slider masterSlider;
+    public Slider SFXSlider;
+    public Slider MusicSlider;
+    
+    [Header("The Value Numbers"), Range(0, 1)]
+    public float masterNum;
+    [Range(0, 1)]
+    public float SFXNum;
+    [Range(0, 1)]
+    public float MusicNum;
+
+    [Header("Audio Sources")]
+    
+    public AudioSource SFXSource;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        AudioListener.volume = masterSlider.value;
+
+        SFXSource.volume = SFXSlider.value;
+
+        musicSource.volume = MusicSlider.value;        
+    }
+
+    public void changeMaster() {
+        masterNum = masterSlider.value;
+    }
+
+    public void changeSFX() {
+        SFXNum = SFXSlider.value;
+    }
+
+    public void changeMusic() {
+        MusicNum = MusicSlider.value;
+    }
+}
