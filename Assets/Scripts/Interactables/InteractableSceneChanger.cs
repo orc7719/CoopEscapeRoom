270a6c76114c79c08ff7f661452d2227a486e599
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-14 19:48:35 +0100

    Added Interactables To Switch Scenes

---
+++ Assets/Scripts/Interactables/InteractableSceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSceneChanger : Interactable
+{
+    [Scene]
+    [SerializeField] string nextScene;
+
+    public override void Interacted(PlayerInteraction player)
+    {
+        SceneLoader.Instance.LoadGameScene(nextScene);
+    }
+}
--- Assets/Scripts/Managers/SceneLoader.cs
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -12,10 +12,91 @@ public class SceneLoader : Singleton<SceneLoader>
     [Scene]
     public string[] survivorScenes;
 
+    string currentScene = "";
+    string newActiveScene = "";
+    bool isLoading = false;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
     public void LoadGameScene(int roleToLoad, int sceneToLoad)
     {
         string sceneName = roleToLoad == 0 ? commanderScene : survivorScenes[sceneToLoad];
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+        StartCoroutine("ChangeGameScene", sceneName);
+    }
+
+    public void LoadGameScene(string sceneToLoad)
+    {
+        Debug.Log("Load");
+        StartCoroutine("ChangeGameScene", sceneToLoad);
+    }
+
+    IEnumerator ChangeGameScene(string newScene)
+    {
+        Debug.Log("Starting Load");
+
+        if (isLoading)
+            yield break;
+
+        isLoading = true;
+
+        currentScene = SceneManager.GetActiveScene().name;
+
+        //Fade to black
+
+        //Unload current scene
+        if(currentScene != "")
+        {
+            AsyncOperation unloadAsync = SceneManager.UnloadSceneAsync(currentScene);
+
+            while (!unloadAsync.isDone)
+            {
+                yield return null;
+            }
+        }
+
+        //Load new scene
+        AsyncOperation loadAsync = SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
+        loadAsync.allowSceneActivation = false;
+        newActiveScene = "";
+
+        while (!loadAsync.isDone)
+        {
+            if(loadAsync.progress >= 0.9f)
+            {
+                loadAsync.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+        //Fade from black
+        isLoading = false;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene Loaded: " + scene.name + ", Index: " + scene.buildIndex);
+
+        if (newActiveScene == "")
+            return;
+
+        Debug.Log("Is active scene");
+
+        Debug.Log("New Active Scene: " + newActiveScene + ", Loaded Scene: " + scene);
+
+        if (SceneManager.GetSceneByName(newActiveScene) == scene)
+        {
+            Debug.Log("Setting active scene");
+            SceneManager.SetActiveScene(scene);
+        }
+
+        newActiveScene = "";
     }
 }
