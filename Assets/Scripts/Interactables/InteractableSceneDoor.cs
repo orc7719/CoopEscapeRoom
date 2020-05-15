ae3e2c3ad95d10c09e3c2e6d1f0ea4e35a75d6c0
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-15 01:21:19 +0100

    Added New Scene Loading Methods
    Scenes can now be loaded via animated door or trigger volume

---
+++ Assets/Scripts/Interactables/InteractableSceneDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using DG.Tweening;
+
+public class InteractableSceneDoor : Interactable
+{
+    [Scene]
+    [SerializeField] string nextScene;
+    [SerializeField] float doorTime = 0.5f;
+    [SerializeField] Transform doorTarget;
+    Transform model;
+
+
+    private void Start()
+    {
+        model = transform.Find("Model");
+    }
+
+    public override InteractState IsInteractable(PlayerInteraction player)
+    {
+        return isInteractable;
+    }
+
+    public override void Interacted(PlayerInteraction player)
+    {
+        StartCoroutine("ChangeSceneAnim");
+    }
+
+    IEnumerator ChangeSceneAnim()
+    {
+        isInteractable = InteractState.Cooldown;
+
+        model.DOLocalMove(doorTarget.localPosition, doorTime);
+        model.DOLocalRotateQuaternion(doorTarget.localRotation, doorTime);
+
+        yield return new WaitForSeconds(doorTime);
+
+        SceneLoader.Instance.LoadGameScene(nextScene);
+    }
+}
---
+++ Assets/Scripts/Interactables/SceneChangeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    [Scene]
+    [SerializeField] string nextScene;
+
+    bool isLoading = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isLoading)
+            return;
+
+        if(other.tag == "Player")
+        {
+            if(other.GetComponent<Player>().isLocalPlayer)
+                SceneLoader.Instance.LoadGameScene(nextScene);
+        }
+    }
+}
