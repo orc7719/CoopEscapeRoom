637dcd9a7c87eee820d2f19c3cb906d833388ec0
orc7719 <2338928+orc7719@users.noreply.github.com>
2020-05-15 19:11:38 +0100

    Added Start State to Toggle Doors

--- Assets/Scripts/DoorToggle.cs
+++ Assets/Scripts/DoorToggle.cs
@@ -11,17 +11,32 @@ public class DoorToggle : MonoBehaviour
     Vector3 startPos;
     Quaternion startRot;
 
+    [SerializeField] bool doorStatus;
+
     [SerializeField] float transitionTime = 2.0f;
 
     private void Start()
     {
         startPos = doorObject.transform.localPosition;
         startRot = doorObject.transform.localRotation;
+
+        MoveDoor(doorStatus, 0);
+    }
+
+    public void ToggleDoor()
+    {
+        MoveDoor(!doorStatus);
     }
 
     public void MoveDoor(bool newValue)
     {
-        doorObject.DOLocalMove(newValue ? targetPos.localPosition : startPos, transitionTime);
-        doorObject.DOLocalRotateQuaternion(newValue ? targetPos.localRotation : startRot, transitionTime);
+        MoveDoor(newValue, 1f);
+    }
+
+    public void MoveDoor(bool newValue, float modifier)
+    {
+        doorStatus = newValue;
+        doorObject.DOLocalMove(newValue ? targetPos.localPosition : startPos, transitionTime * modifier);
+        doorObject.DOLocalRotateQuaternion(newValue ? targetPos.localRotation : startRot, transitionTime * modifier);
     }
 }
